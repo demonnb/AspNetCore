23260fdbb2267f5e67ed018fc5cc7e2c458563a1
Alain Belleville <alain.belleville@outlook.com>
2018-12-05 14:12:38 -0400

    init commit

---
+++ src/AspCoreMVC/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspCoreMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(ConfigConfiguration)
+                .UseStartup<Startup>();
+
+        static void ConfigConfiguration(WebHostBuilderContext webHostBuilderContext, IConfigurationBuilder configurationBuilder)
+            {
+                configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("azureKeyVault.json", false, true)
+                    .AddEnvironmentVariables();
+
+                var config = configurationBuilder.Build();
+
+                configurationBuilder.AddAzureKeyVault(
+                    $"https://{config["vault"]}.vault.azure.net/",
+                    config["clientId"],
+                    config["clientSecret"]
+                );
+            }
+
+    }
+}
